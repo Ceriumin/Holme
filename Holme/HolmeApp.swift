d4b9a66f97b3643ace35582b8c562bed78e6872d
Ceriumin <barylagracjan@icloud.com>
2025-05-19 13:44:49 +0100

    chore: initial commit

---
+++ Holme/HolmeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HolmeApp.swift
+//  Holme
+//
+//  Created by Gracjan Baryla on 19/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct HolmeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HolmeTests/HolmeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HolmeTests.swift
+//  HolmeTests
+//
+//  Created by Gracjan Baryla on 19/05/2025.
+//
+
+import Testing
+@testable import Holme
+
+struct HolmeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
